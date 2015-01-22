f074ff6a0609c4574bca2f6b909b354024cdb32d
Imatation <ludo.nuskin@yahoo.fr>
2015-01-22 17:54:29 +0100

    Update Program.cs

--- AutoLevelSpells/Program.cs
+++ AutoLevelSpells/Program.cs
@@ -47,7 +47,7 @@ namespace AutoLevelSpells
                     sequence = new[] {1, 3, 1, 2, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 2, 2};
                     break;
                 case "Akali":
-                    sequence = new[] {1, 2, 1, 3, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2};
+                    sequence = new[] {1, 3, 2, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2};
                     break;
                 case "Alistar":
                     sequence = new[] {1, 3, 2, 1, 3, 4, 1, 3, 1, 3, 4, 1, 3, 2, 2, 4, 2, 2};
@@ -56,7 +56,7 @@ namespace AutoLevelSpells
                     sequence = new[] {2, 3, 3, 1, 3, 4, 3, 1, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2};
                     break;
                 case "Anivia":
-                    sequence = new[] {1, 3, 1, 3, 3, 4, 3, 2, 3, 2, 4, 1, 1, 1, 2, 4, 2, 2};
+                    sequence = new[] {1, 3, 3, 2, 3, 4, 3, 1, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2};
                     break;
                 case "Annie":
                     sequence = new[] {2, 1, 1, 3, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3};
@@ -302,7 +302,7 @@ namespace AutoLevelSpells
                     sequence = new[] {1, 3, 3, 2, 3, 4, 3, 1, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2};
                     break;
                 case "Sivir":
-                    sequence = new[] {2, 1, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3};
+                    sequence = new[] {1, 3, 2, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3};
                     break;
                 case "Skarner":
                     sequence = new[] {1, 2, 1, 2, 1, 4, 1, 2, 1, 2, 4, 2, 3, 3, 3, 4, 3, 3};
