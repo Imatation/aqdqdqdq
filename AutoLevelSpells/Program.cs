74892bfe03c3a370da8151e18d12bbbbdbcb4bea
Imatation <ludo.nuskin@yahoo.fr>
2015-01-24 17:17:30 +0100

    Update Program.cs

--- AutoLevelSpells/Program.cs
+++ AutoLevelSpells/Program.cs
@@ -332,7 +332,7 @@ namespace AutoLevelSpells
                     sequence = new[] {1, 3, 2, 2, 2, 4, 2, 3, 2, 3, 4, 3, 3, 1, 1, 4, 1, 1};
                     break;
                 case "Tristana":
-                    sequence = new[] {3, 2, 2, 3, 2, 4, 2, 1, 2, 1, 4, 1, 1, 1, 3, 4, 3, 3};
+                    sequence = new[] {3, 2, 3, 1, 3, 4, 1, 3, 1, 1, 4, 3, 1, 2, 2, 4, 2, 2};
                     break;
                 case "Trundle":
                     sequence = new[] {1, 2, 1, 3, 1, 4, 1, 2, 1, 3, 4, 2, 3, 2, 3, 4, 2, 3};
